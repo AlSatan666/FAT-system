e4c99f0395f2442c0f0644d06ec1090b8e84eb73
AlSatan666 <145715865+AlSatan666@users.noreply.github.com>
2024-07-05 13:07:11 +0200

    Add files via upload

---
+++ FAT/file_system.h
@@ -0,0 +1,41 @@
+#ifndef FILE_SYSTEM_H
+#define FILE_SYSTEM_H
+
+#define MAX_FILENAME_LENGTH 256
+#define MAX_FILE_SIZE 1024
+
+typedef struct Directory Directory;
+
+typedef struct DirectoryEntry {
+    char name[MAX_FILENAME_LENGTH];
+    int position;
+    int is_dir; // Flag per directory
+    Directory* subdir; // Puntatore alle subdiretory(ricorda)
+} DirectoryEntry;
+
+struct Directory {
+    DirectoryEntry* entries;
+    int num_entries;
+    Directory* parent; //vedi sopra
+};
+
+typedef struct {
+    char* buffer;
+    int buffer_size;
+    Directory* root;
+    Directory* current_directory;
+} FileSystem;
+
+void createFile(FileSystem* fs, const char* filename);
+void eraseFile(FileSystem* fs, const char* filename);
+void write(FileSystem* fs, const char* filename, const char* data);
+void read(FileSystem* fs, const char* filename, char* data, int size);
+void seek(FileSystem* fs, const char* filename, int position);
+void createDir(FileSystem* fs, const char* dirname);
+void eraseDir(FileSystem* fs, const char* dirname);
+void changeDir(FileSystem* fs, const char* dirname);
+void listDir(FileSystem* fs);
+void listFiles(FileSystem* fs);
+void printCurrentDir(FileSystem* fs);
+
+#endif
