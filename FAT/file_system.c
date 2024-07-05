68b476bd41039b6a88bed0007ec592b9a34712be
AlSatan666 <145715865+AlSatan666@users.noreply.github.com>
2024-07-05 15:14:08 +0200

    Update file_system.c +erase files + seek files + changed create files for the seek

--- FAT/file_system.c
+++ FAT/file_system.c
@@ -7,7 +7,14 @@ void createFile(FileSystem* fs, const char* filename) {
     printf("Attempting to create file: %s\n", filename);
     DirectoryEntry* entries = fs->current_directory->entries;
     int num_entries = fs->current_directory->num_entries;
-    
+
+    for (int i = 0; i < num_entries; ++i) {
+        if (strcmp(entries[i].name, filename) == 0) {
+            printf("\nFile already exists.\n");
+            return;
+        }
+    }
+
     int position = -1;
     for (int i = 0; i < fs->buffer_size; i += MAX_FILE_SIZE) {
         int is_free = 1;
@@ -43,11 +50,34 @@ void createFile(FileSystem* fs, const char* filename) {
 
     fs->current_directory->entries = entries;
     fs->current_directory->num_entries++;
-    printf("Successfully created file: %s\n", filename);
+    printf("Successfully created file: %s \n", filename);
 }  
 
 void eraseFile(FileSystem* fs, const char* filename) {
-}   
+    printf("Attempting to erase file: %s\n", filename);
+    DirectoryEntry* entries = fs->current_directory->entries;
+    int num_entries = fs->current_directory->num_entries;
+
+    for (int i = 0; i < num_entries; ++i) {
+        if (strcmp(entries[i].name, filename) == 0 && !entries[i].is_dir) {
+            memset(fs->buffer + entries[i].position, 0, MAX_FILE_SIZE);
+
+            memmove(&entries[i], &entries[i + 1], (num_entries - i - 1) * sizeof(DirectoryEntry));
+            entries = realloc(entries, (num_entries - 1) * sizeof(DirectoryEntry));
+            if (entries == NULL && num_entries > 1) {
+                printf("\nMemory reallocation failed.\n");
+                return;
+            }
+
+            fs->current_directory->entries = entries;
+            fs->current_directory->num_entries--;
+            printf("Successfully erased file: %s\n", filename);
+            return;
+        }
+    }
+
+    printf("\nFile not found.\n");
+}
 
 void write(FileSystem* fs, const char* filename, const char* data) {
 }  
@@ -55,7 +85,19 @@ void write(FileSystem* fs, const char* filename, const char* data) {
 void read(FileSystem* fs, const char* filename, char* data, int size) {
 }  
 
-void seek(FileSystem* fs, const char* filename, int position) {
+void seek(FileSystem* fs, const char* filename) {
+    printf("Attempting to seek file: %s\n", filename);
+    DirectoryEntry* entries = fs->current_directory->entries;
+    int num_entries = fs->current_directory->num_entries;
+
+    for (int i = 0; i < num_entries; ++i) {
+        if (strcmp(entries[i].name, filename) == 0 && !entries[i].is_dir) {
+            printf("The position of file '%s' is %d.\n", filename, entries[i].position);
+            return;
+        }
+    }
+
+    printf("\nFile not found.\n");
 }
 
 void createDir(FileSystem* fs, const char* dirname) {
