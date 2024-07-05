ff17454364f07698067df5947e8a65c307f0f9cd
AlSatan666 <145715865+AlSatan666@users.noreply.github.com>
2024-07-05 18:15:09 +0200

    Update main.c

--- FAT/main.c
+++ FAT/main.c
@@ -1,21 +1,30 @@
+#include <stdio.h>
+#include <string.h>
+#include <sys/mman.h>
+#include <unistd.h>
 #include "file_system.h"
 
 int main() {
-    initialize_filesystem("filesystem.dat");
+    FileSystem fs;
+    initialize_filesystem(&fs, "filesystem.dat", 1024);
 
-    FileHandle *fh = createFile("file1.txt");
-    if (fh != NULL) {
-        const char *text = "Hello, File System!";
-        writeFile(fh, text, strlen(text));
-    }
+    create_file(&fs, "test.txt");
 
-    createDir("dir1");
-    changeDir("dir1");
-    createFile("file2.txt");
+    FileHandle fh = {fs.root_dir[0].first_block, 0};
+    const char *data = "Hello, World!";
+    write_file(&fs, &fh, data, strlen(data));
 
-    changeDir("/");
+    char buffer[20];
+    fh.position = 0;
+    read_file(&fs, &fh, buffer, strlen(data));
+    buffer[strlen(data)] = '\0';
+    printf("Read data: %s\n", buffer);
 
-    listDir();
+    create_dir(&fs, "docs");
+    change_dir(&fs, "docs");
+    list_dir(&fs);
 
+    munmap(fs.data, fs.num_blocks * BLOCK_SIZE);
+    close(fs.fd);
     return 0;
 }
