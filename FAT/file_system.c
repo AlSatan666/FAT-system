9feaf384c0e0f42898434bf9019b5800ac5a8c67
AlSatan666 <145715865+AlSatan666@users.noreply.github.com>
2024-07-05 13:26:25 +0200

    Update file_system.c+ creata position & entries

--- FAT/file_system.c
+++ FAT/file_system.c
@@ -4,7 +4,43 @@
 #include <string.h>
 
 void createFile(FileSystem* fs, const char* filename) {
-}   
+    int position = -1;
+    for (int i = 0; i < fs->buffer_size; i += MAX_FILE_SIZE) {
+        int is_free = 1;
+        for (int j = 0; j < MAX_FILE_SIZE; ++j) {
+            if (fs->buffer[i + j] != '\0') {
+                is_free = 0;
+                break;
+            }
+        }
+        if (is_free) {
+            position = i;
+            break;
+        }
+    }
+
+    if (position == -1) {
+        printf("\nNo free space available.\n");
+        return;
+    }
+
+    DirectoryEntry new_entry;
+    strcpy(new_entry.name, filename);
+    new_entry.position = position;
+    new_entry.is_dir = 0;
+    new_entry.subdir = NULL;
+
+    entries = realloc(entries, (num_entries + 1) * sizeof(DirectoryEntry));
+    if (entries == NULL) {
+        printf("\nMemory allocation failed.\n");
+        return;
+    }
+    entries[num_entries] = new_entry;
+
+    fs->current_directory->entries = entries;
+    fs->current_directory->num_entries++;
+    printf("Successfully created file: %s\n", filename);
+}  
 
 void eraseFile(FileSystem* fs, const char* filename) {
 }   
