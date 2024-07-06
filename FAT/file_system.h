c82fff8aab24d205add48a254c5eaf4d1beb1339
AlSatan666 <145715865+AlSatan666@users.noreply.github.com>
2024-07-06 12:00:04 +0200

    Update file_system.h
    revisione e ripartenza da zero

--- FAT/file_system.h
+++ FAT/file_system.h
@@ -1,49 +1,10 @@
 #ifndef FILE_SYSTEM_H
 #define FILE_SYSTEM_H
-#ifndef _SSIZE_T_DEFINED
-typedef long ssize_t;
-#define _SSIZE_T_DEFINED
-#endif
 
 #include <stdint.h>
-#include <stddef.h>
 
-#define BLOCK_SIZE 4096
-#define MAX_FILENAME_LENGTH 255
+int initialize_fs(const char *filename, uint32_t size);
+int create_file(const char *filename);
+void uninitialize_fs();
 
-typedef struct {
-    int32_t next_block;
-} FATEntry;
-
-typedef struct {
-    char name[MAX_FILENAME_LENGTH];
-    int8_t is_directory;
-    int32_t first_block;
-} DirectoryEntry;
-
-typedef struct {
-    int32_t block_index;
-    int32_t position;
-} FileHandle;
-
-typedef struct {
-    int fd;
-    uint8_t *data;
-    FATEntry *fat;
-    DirectoryEntry *root_dir;
-    int num_blocks;
-    int fat_size;
-} FileSystem;
-
-void initialize_filesystem(FileSystem *fs, const char *file_path, int num_blocks);
-int create_file(FileSystem *fs, const char *name);
-int erase_file(FileSystem *fs, const char *name);
-ssize_t write_file(FileSystem *fs, FileHandle *fh, const void *data, size_t size);
-ssize_t read_file(FileSystem *fs, FileHandle *fh, void *buffer, size_t size);
-int seek_file(FileSystem *fs, FileHandle *fh, int32_t position);
-int create_dir(FileSystem *fs, const char *name);
-int erase_dir(FileSystem *fs, const char *name);
-int change_dir(FileSystem *fs, const char *path);
-void list_dir(FileSystem *fs);
-
-#endif
+#endif // FILE_SYSTEM_H
