c87b2fd995c995abe835a16e2c8c6be91ed25a4d
AlSatan666 <145715865+AlSatan666@users.noreply.github.com>
2024-07-07 14:54:38 +0200

    Update main.c

--- FAT/main.c
+++ FAT/main.c
@@ -1,5 +1,15 @@
 #include "file_system.h"
 #include <stdio.h>
+#include <string.h>
+
+void test_create_file();
+void test_erase_file();
+void test_write_read_file();
+void test_seek_file();
+void test_create_dir();
+void test_erase_dir();
+void test_change_dir();
+void test_list_dir();
 
 int main(int argc, char** argv) {
     if (argc < 3) {
@@ -15,98 +25,129 @@ int main(int argc, char** argv) {
         return res;
     }
 
-    res = create_dir("TEST");
-    if (res) {
-        printf("Error creating directory 'TEST'\n");
-        goto cleanup;
-    }
-
-    ls();
-
-    res = cd("TEST");
-    if (res) {
-        printf("Error changing to directory 'TEST'\n");
-        goto cleanup;
-    }
-    char file_content[12] = "helloworld1";
-
-    res = create_file("HELLO", "TXT", 12, file_content);
-    if (res) {
-        printf("Error creating file 'HELLO.TXT'\n");
-        goto cleanup;
-    }
-
-    ls();
-
-    res = create_file("HELLO2", "IMG", 12, file_content);
-    if (res) {
-        printf("Error creating file 'HELLO2.IMG'\n");
-        goto cleanup;
-    }
-
-    res = create_dir("DELME");
-    if (res) {
-        printf("Error creating directory 'DELME'\n");
-        goto cleanup;
-    }
-
-    res = cd("DELME");
-    if (res) {
-        printf("Error changing to directory 'DELME'\n");
-        goto cleanup;
-    }
-
-    res = create_file("HELLO3", "IMG", 12, file_content);
-    if (res) {
-        printf("Error creating file 'HELLO3.IMG'\n");
-        goto cleanup;
-    }
-    res = create_file("HELLO4", "IMG", 12, file_content);
-    if (res) {
-        printf("Error creating file 'HELLO4.IMG'\n");
-        goto cleanup;
-    }
-
-    ls();
-
-    res = cd("..");
-    if (res) {
-        printf("Error changing to parent directory\n");
-        goto cleanup;
-    }
-
-    ls();
-
-    res = cd("..");
-    if (res) {
-        printf("Error changing to root directory\n");
-        goto cleanup;
-    }
-
-    res = remove_dir("TEST", 1);
-    if (res) {
-        printf("Error erasing directory 'TEST'\n");
-        goto cleanup;
-    }
-
-    ls();
-
-cleanup:
-    if (res == DIR_CREATE_ERROR) {
-        printf("Cannot create new directory in %s\n", get_current_dir()->name);
-        return -1;
-    }
-    if (res == FILE_CREATE_ERROR) {
-        printf("Cannot create file in directory %s \n", get_current_dir()->name);
-        return -1;
-    }
-    if (res == FILE_NOT_FOUND) {
-        printf("File/Directory not found in directory %s \n", get_current_dir()->name);
-    }
-    if (res == INIT_ERROR) {
-        printf("Initialize error\n");
-        return -1;
-    }
+    test_create_file();
+    test_erase_file();
+    test_write_read_file();
+    test_seek_file();
+    test_create_dir();
+    test_erase_dir();
+    test_change_dir();
+    test_list_dir();
 
     return 0;
 }
+
+void test_create_file() {
+    printf("\nRunning test: create_file\n");
+    char file_content[12] = "helloworld1";
+    int res = create_file("TEST", "TXT", 12, file_content);
+    if (res == 0) {
+        printf("File 'TEST.TXT' created successfully.\n");
+    } else {
+        printf("Error creating file 'TEST.TXT'.\n");
+    }
+}
+
+void test_erase_file() {
+    printf("\nRunning test: erase_file\n");
+    int res = remove_file("TEST", "TXT");
+    if (res == 0) {
+        printf("File 'TEST.TXT' erased successfully.\n");
+    } else {
+        printf("Error erasing file 'TESTFILE.TXT'.\n");
+    }
+}
+
+void test_write_read_file() {
+    printf("\nRunning test: write_read_file\n");
+    char file_content[20] = "Hello FAT World!";
+    int res = create_file("WRITE", "TXT", 17, file_content);
+    if (res == 0) {
+        printf("File 'WRITE.TXT' created successfully.\n");
+    } else {
+        printf("Error creating file 'WRITEFILE.TXT'.\n");
+    }
+
+    char buffer[20];
+    int bytes_read = read_file_content("WRITEFILE", "TXT", buffer);
+    if (bytes_read == 17) {
+        buffer[bytes_read] = '\0';
+        printf("Read from 'WRITEFILE.TXT': %s\n", buffer);
+    } else {
+        printf("Error reading file 'WRITEFILE.TXT'.\n");
+    }
+}
+
+void test_seek_file() {
+    printf("\nRunning test: seek_file\n");
+    FileHandle file_handle;
+    file_handle.file_entry = locate_file("WRITEFILE", "TXT", 0);
+    if (file_handle.file_entry == NULL) {
+        printf("File 'WRITEFILE.TXT' not found.\n");
+        return;
+    }
+    file_handle.position = 5;
+
+    char buffer[20];
+    memset(buffer, 0, sizeof(buffer));
+    int bytes_read = read_file_content(file_handle.file_entry->name, file_handle.file_entry->extension, buffer);
+    if (bytes_read > 0) {
+        buffer[bytes_read] = '\0';
+        printf("Read from position %d in 'WRITE.TXT': %s\n", file_handle.position, buffer + file_handle.position);
+    } else {
+        printf("Error seeking in file 'WRITE.TXT'.\n");
+    }
+}
+
+
+void test_create_dir() {
+    printf("\nRunning test: create_dir\n");
+    int res = create_dir("TESTDIR");
+    if (res == 0) {
+        printf("Directory 'TESTDIR' created successfully.\n");
+    } else {
+        printf("Error creating directory 'TESTDIR'.\n");
+    }
+}
+
+void test_erase_dir() {
+    printf("\nRunning test: erase_dir\n");
+    int res = remove_dir("TESTDIR", 0);
+    if (res == 0) {
+        printf("Directory 'TESTDIR' erased successfully.\n");
+    } else {
+        printf("Error erasing directory 'TESTDIR'.\n");
+    }
+}
+
+void test_change_dir() {
+    printf("\nRunning test: change_dir\n");
+    int res = create_dir("DIRCHANGE");
+    if (res == 0) {
+        printf("Directory 'DIRCHANGE' created successfully.\n");
+    } else {
+        printf("Error creating directory 'DIRCHANGE'.\n");
+    }
+
+    res = cd("DIRCHANGE");
+    if (res == 0) {
+        printf("Changed to directory 'DIRCHANGE' successfully.\n");
+    } else {
+        printf("Error changing to directory 'DIRCHANGE'.\n");
+    }
+
+    res = cd("..");
+    if (res == 0) {
+        printf("Changed to parent directory successfully.\n");
+    } else {
+        printf("Error changing to parent directory.\n");
+    }
+}
+
+void test_list_dir() {
+    printf("\nRunning test: list_dir\n");
+    printf("Contents of root directory:\n");
+    ls();
+}
+
+
