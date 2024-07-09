143ab88ef5b6df2d77337d5b1cdf28ce8c38e429
AlSatan666 <145715865+AlSatan666@users.noreply.github.com>
2024-07-09 17:41:40 +0200

    Update test_file_system.c+save and load

--- FAT/test_file_system.c
+++ FAT/test_file_system.c
@@ -2,13 +2,11 @@
 #include <string.h>
 #include "file_system.h"
 
-// Definizione dei percorsi per i file FAT e DATA
-#define FAT_FILE "main_fat_file.dat"
-#define DATA_FILE "main_data_file.dat"
+#define DATATICUS_FILE "DATATICUS.dat"
 
-// Funzione di test per l'inizializzazione del file system
+
 void test_fs_initialize() {
-    int res = fs_initialize(FAT_FILE, DATA_FILE);
+    int res = fs_initialize(DATATICUS_FILE);
     if (res == 0) {
         printf("fs_initialize: PASSED\n");
     } else {
@@ -16,7 +14,37 @@ void test_fs_initialize() {
     }
 }
 
-// Funzione di test per la creazione di una directory
+void test_fs_load() {
+    int res = fs_load(DATATICUS_FILE);
+    if (res == 0) {
+        printf("fs_load: PASSED\n");
+        printf("fs_load: Loaded file system from DATATICUS file.\n");
+        printf("fs_load: Current directory: %s\n", fs->current_directory);
+        printf("fs_load: FAT entries: %d\n", fs->fat_entries);
+        printf("fs_load: Data size: %d bytes\n", fs->data_size);
+    } else {
+        printf("fs_load: FAILED\n");
+    }
+}
+
+
+void test_fs_save() {
+    int res = fs_save();
+    if (res == 0) {
+        printf("fs_save: PASSED\n");
+        printf("fs_save: Successfully wrote FileSystem struct to file\n");
+        printf("fs_save: Successfully wrote FAT table to file\n");
+        printf("fs_save: Successfully wrote data blocks to file\n");
+        printf("fs_save: File system successfully saved to disk.\n");
+        printf("fs_save: Current directory: %s\n", fs->current_directory);
+        printf("fs_save: FAT entries: %d\n", fs->fat_entries);
+        printf("fs_save: Data size: %d bytes\n", fs->data_size);
+    } else {
+        printf("fs_save: FAILED\n");
+    }
+}
+
+
 void test_create_dir() {
     int res = create_dir("TESTDIR");
     if (res == 0) {
@@ -26,7 +54,6 @@ void test_create_dir() {
     }
 }
 
-// Funzione di test per la creazione di un file
 void test_create_file() {
     char content[] = "Hello, World!";
     int res = create_file("TEST", "TXT", strlen(content), content);
@@ -37,7 +64,6 @@ void test_create_file() {
     }
 }
 
-// Funzione di test per il cambiamento della directory corrente
 void test_cd() {
     int res = cd("TESTDIR");
     if (res == 0) {
@@ -47,7 +73,6 @@ void test_cd() {
     }
 }
 
-// Funzione di test per ritornare alla directory radice
 void test_cd_root() {
     int res = cd("..");
     if (res == 0) {
@@ -57,13 +82,11 @@ void test_cd_root() {
     }
 }
 
-// Funzione di test per elencare il contenuto della directory corrente
 void test_ls() {
     printf("ls: ");
     ls();
 }
 
-// Funzione per la rimozione di un file
 void test_remove_file() {
     int res = remove_file("TEST", "TXT");
     if (res == 0) {
@@ -73,9 +96,8 @@ void test_remove_file() {
     }
 }
 
-// Funzione per la rimozione di una directory
 void test_remove_dir() {
-    int res = remove_dir("TESTDIR", 1); // Il secondo parametro indica se la rimozione è ricorsiva
+    int res = remove_dir("TESTDIR", 1);
     if (res == 0) {
         printf("remove_dir: PASSED\n");
     } else {
@@ -83,12 +105,11 @@ void test_remove_dir() {
     }
 }
 
-// Funzione per scrivere contenuto in un file
 void test_write_file() {
     printf("\nRunning test: write_file\n");
     char additional_content[] = "spazio sprecato";
     printf("test_write_file: Writing 'spazio sprecato' to 'TEST.TXT' at the end\n");
-    int bytes_written = write_file_content("TEST", "TXT", additional_content, -1, 15); // -1 indica scrittura alla fine del file
+    int bytes_written = write_file_content("TEST", "TXT", additional_content, -1, 15);
     if (bytes_written == 15) {
         printf("Written to 'TEST.TXT' successfully.\n");
     } else {
@@ -96,11 +117,9 @@ void test_write_file() {
     }
 }
 
-// Funzione per leggere il contenuto di un file
 void test_read_file(const char* name, const char* ext) {
     printf("\nRunning test: read_file\n");
 
-    // Creazione e inizializzazione di un handle per il file
     FileHandle file_handle;
     file_handle.file_entry = locate_file(name, ext, 0);
     if (file_handle.file_entry == NULL) {
@@ -109,7 +128,6 @@ void test_read_file(const char* name, const char* ext) {
     }
     file_handle.position = 0;
 
-    // Lettura del contenuto del file
     char buffer[50];
     int bytes_read = read_file_content(&file_handle, buffer, sizeof(buffer) - 1);
     if (bytes_read > 0) {
@@ -120,11 +138,9 @@ void test_read_file(const char* name, const char* ext) {
     }
 }
 
-// Funzione per cercare una posizione specifica in un file
 void test_seek_file() {
     printf("\nRunning test: seek_file\n");
 
-    // Creazione e inizializzazione di un handle per il file
     FileHandle file_handle;
     file_handle.file_entry = locate_file("TEST", "TXT", 0);
     if (file_handle.file_entry == NULL) {
@@ -133,7 +149,7 @@ void test_seek_file() {
     }
     file_handle.position = 0;
 
-    // Seek dalla posizione 5 dall'inizio
+    // Seek to position 5 from the beginning
     int res = seek_file(&file_handle, 5, SEEK_SET);
     if (res == 0) {
         printf("Seek to position 5 in 'TEST.TXT' successful. Current position: %d\n", file_handle.position);
@@ -141,14 +157,14 @@ void test_seek_file() {
         printf("Seek to position 5 in 'TEST.TXT' failed.\n");
     }
 
-    // Lettura dal punto corrente
+    // Read from the current position
     char buffer[40];
     memset(buffer, 0, sizeof(buffer));
     int bytes_read = read_file_content(&file_handle, buffer, sizeof(buffer));
     buffer[bytes_read] = '\0';
     printf("Reading from position %d in 'TEST.TXT': %s\n", file_handle.position, buffer);
 
-    // Seek 3 posizioni in avanti dal punto corrente
+    // Seek forward by 3 from the current position
     res = seek_file(&file_handle, 3, SEEK_CUR);
     if (res == 0) {
         printf("Seek forward by 3 in 'TEST.TXT' successful. Current position: %d\n", file_handle.position);
@@ -156,13 +172,13 @@ void test_seek_file() {
         printf("Seek forward by 3 in 'TEST.TXT' failed.\n");
     }
 
-    // Lettura dal punto corrente
+    // Read from the current position
     memset(buffer, 0, sizeof(buffer));
     bytes_read = read_file_content(&file_handle, buffer, sizeof(buffer));
     buffer[bytes_read] = '\0';
     printf("Reading from position %d in 'TEST.TXT': %s\n", file_handle.position, buffer);
 
-    // Seek 2 posizioni indietro dal punto corrente
+    // Seek backward by 2 from the current position
     res = seek_file(&file_handle, -2, SEEK_CUR);
     if (res == 0) {
         printf("Seek backward by 2 in 'TEST.TXT' successful. Current position: %d\n", file_handle.position);
@@ -170,13 +186,13 @@ void test_seek_file() {
         printf("Seek backward by 2 in 'TEST.TXT' failed.\n");
     }
 
-    // Lettura dal punto corrente
+    // Read from the current position
     memset(buffer, 0, sizeof(buffer));
     bytes_read = read_file_content(&file_handle, buffer, sizeof(buffer));
     buffer[bytes_read] = '\0';
     printf("Reading from position %d in 'TEST.TXT': %s\n", file_handle.position, buffer);
 
-    // Seek alla fine del file
+    // Seek to the end of the file
     res = seek_file(&file_handle, 0, SEEK_END);
     if (res == 0) {
         printf("Seek to end of 'TEST.TXT' successful. Current position: %d\n", file_handle.position);
@@ -184,10 +200,9 @@ void test_seek_file() {
         printf("Seek to end of 'TEST.TXT' failed.\n");
     }
 
-    // Lettura dal punto corrente
+    // Read from the current position
     memset(buffer, 0, sizeof(buffer));
     bytes_read = read_file_content(&file_handle, buffer, sizeof(buffer));
     buffer[bytes_read] = '\0';
     printf("Reading from position %d in 'TEST.TXT': %s\n", file_handle.position, buffer);
 }
-
